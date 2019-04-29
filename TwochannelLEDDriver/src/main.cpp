eedfdcfde7defeda0c8ee4c84053582279c51bf0
Pascal Pieper <Accounts@PascalPieper.de>
2019-04-29 23:15:11 +0200

    RTC works with sunMoon shit

--- TwochannelLEDDriver/src/main.cpp
+++ TwochannelLEDDriver/src/main.cpp
@@ -6,33 +6,35 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <string.h>
-#include "Wire.h"
+#include <Wire.h>
+#include <Time.h>
+#include <TimeLib.h>
+#include <DS3232RTC.h>
+#include <sunMoon.h>
+
+#define OUR_latitude    53.075600
+#define OUR_longtitude  8.801781
+#define OUR_timezone    120                     // localtime with UTC difference in minutes
 #define DS3231_ADDRESSE 0x68
 
 
 const char * ssid = "BehindertesWLANFuerDenDrucker";
 const char * password = "0~0~0}{0~";
 
-#define LINKS 2
-#define RECHTS 0
+#define WARM 2
+#define KALT 0
+#define AW_MAX 1024
 
-uint16_t l = 0, r = 512;
+uint16_t kalt = 0, warm = 0;
 bool startup = true;
 
 ESP8266WebServer server(80);
+DS3232RTC RTC(false);
+sunMoon sm;
 IPAddress myIp;
 
 void blinkIP();
 void handleAndDelay(uint16_t wait);
-byte decToBcd(byte val) {
-// Dezimal Zahl zu binary coded decimal (BCD) umwandeln
-  return((val/10*16) + (val%10));
-}
-byte bcdToDec(byte val) {
-// BCD (binary coded decimal) in Dezimal Zahl umwandeln
-  return((val/16*10) + (val%16));
-}
-
 void handleNotFound(){
   String message = "File Not Found\n\n";
   message += "URI: ";
@@ -47,59 +49,61 @@ void handleNotFound(){
   }
   server.send(404, "text/plain", message);
 }
+void setLight();
 
 void setup() {
-    pinMode(LINKS, OUTPUT);
-    pinMode(RECHTS, OUTPUT);
+    pinMode(WARM, OUTPUT);
+    pinMode(KALT, OUTPUT);
 
-    analogWrite(LINKS, l);
-    analogWrite(RECHTS, r);
+    analogWrite(WARM, warm = 0);
+    analogWrite(KALT, kalt = 0);
 
     Wire.begin(1, 3);
+
+    setSyncProvider(RTC.get);                     // the function to get the time from the RTC
+    if (timeStatus() != timeSet)
+      return;
+    sm.init(OUR_timezone, OUR_latitude, OUR_longtitude);
+    setLight();
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     while(WiFi.status() != WL_CONNECTED)
     {
         delay(500);
+        /*
         l = l ? 0 : 512;
         r = r ? 0 : 512;
-        analogWrite(LINKS, l);
-        analogWrite(RECHTS, r);
+        analogWrite(WARM, l);
+        analogWrite(KALT, r);
+        */
+
     }
 
-    analogWrite(LINKS, l = 0);
-    analogWrite(RECHTS, r = 0);
+    //analogWrite(WARM, l = 0);
+    //analogWrite(KALT, r = 0);
     MDNS.begin("MultiLED");
     myIp = WiFi.localIP();
+    startup = false;        //to prevent blinking the IP
     server.on("/", []()
     {
-        startup = false;
-        if(server.hasArg("l"))
-        {
-            l = std::strtoul(server.arg("l").c_str(), nullptr, 10);
-        }
-        if(server.hasArg("r"))
-        {
-            r = std::strtoul(server.arg("r").c_str(), nullptr, 10);
-        }
-        Wire.beginTransmission(DS3231_ADDRESSE);
-        Wire.write(0); // DS3231 Register zu 00h
-        Wire.endTransmission();
-        Wire.requestFrom(DS3231_ADDRESSE, 7); // 7 Byte Daten vom DS3231 holen
-        byte sekunde = bcdToDec(Wire.read() & 0x7f);
-        bcdToDec(Wire.read());
-        bcdToDec(Wire.read() & 0x3f);
-        bcdToDec(Wire.read());
-        bcdToDec(Wire.read());
-        bcdToDec(Wire.read());
-        bcdToDec(Wire.read());
 
-        char answer[50] ;
-        sprintf(answer, "l: %04d r: %04d\nSekunden: %d", l, r, sekunde);
+        if(server.hasArg("warm"))
+        {
+            warm = std::strtoul(server.arg("warm").c_str(), nullptr, 10);
+        }
+        if(server.hasArg("kalt"))
+        {
+            kalt = std::strtoul(server.arg("kalt").c_str(), nullptr, 10);
+        }
+        time_t myTime = RTC.get();
+        time_t sRise = sm.sunRise();
+        time_t sSet  = sm.sunSet();
+        char answer[100] ;
+        sprintf(answer, "warm: %04d kalt: %04d\nnow: %lu, rise: %lu, set: %lu", warm, kalt, myTime, sRise, sSet);
 
-        analogWrite(LINKS, l);
-        analogWrite(RECHTS, r);
+        analogWrite(WARM, warm);
+        analogWrite(KALT, kalt);
         server.send(200, "text/plain", answer);
     });
 
@@ -107,9 +111,10 @@ void setup() {
     {
         if(server.hasArg("ts"))
         {
-            unsigned long newTS = std::strtoul(server.arg("ts").c_str(), nullptr, 10);
+            time_t ts = std::strtoul(server.arg("ts").c_str(), nullptr, 10);
             char answer[50] ;
-            sprintf(answer, "new timestamp: %d", ts);
+            sprintf(answer, "new timestamp: %lu", ts);
+            RTC.set(ts);
             server.send(200, "text/plain", answer);
         }
         server.send(200, "text/plain", "no timestamp");
@@ -123,21 +128,26 @@ void loop() {
     server.handleClient();
     if(startup)
         blinkIP();
+    static uint32_t counter = 0;
+    if(counter++ > 0xFFFFF)
+    {
+        setLight();
+    }
 }
 
 void blinkIP()
 {
-    analogWrite(LINKS, 0);
-    analogWrite(RECHTS, 0);
+    analogWrite(WARM, 0);
+    analogWrite(KALT, 0);
     while(startup)
     {
         for(uint8_t i = 0; i < myIp[3]; i++)
         {
-            analogWrite(LINKS, 250);
-            analogWrite(RECHTS, 250);
+            analogWrite(WARM, 250);
+            analogWrite(KALT, 250);
             handleAndDelay(200);
-            analogWrite(LINKS, 0);
-            analogWrite(RECHTS, 0);
+            analogWrite(WARM, 0);
+            analogWrite(KALT, 0);
             handleAndDelay(200);
             if(!startup)
                 return;
@@ -153,5 +163,21 @@ void handleAndDelay(uint16_t wait)
     while(millis() < target)
     {
         server.handleClient();
+    }
+}
+
+
+void setLight()
+{
+    analogWrite(WARM, warm = AW_MAX);
+    time_t now = RTC.get();
+    if(now > sm.sunRise() && now < sm.sunSet())
+    {
+        //hell!
+        analogWrite(KALT, kalt = AW_MAX);
+    }
+    else
+    {
+        analogWrite(KALT, kalt = 0);
     }
 }
